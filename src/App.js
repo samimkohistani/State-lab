58ec0acbfbcf16a7ae29ae1eeb50362ac8df3c55
Samim Kohistani <samimkohistani@Samims-MacBook-Air.local>
2022-09-22 11:51:19 -0400

    first and last

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
+import imagesArr from './ImageData';
 
 function App() {
+const [bigImage, setBigImage] = useState(imagesArr[0].img);
+
+const handleClick = (imgUrl) =>{setBigImage(imgUrl)};
+
+const images = imagesArr.map((image, index)=>{
+  return(
+    <img className="thumb"
+    src={image.img}
+    alt={image.city}
+    key={index}
+    onClick={() =>{handleClick(image.img)}}
+    />
+  );
+});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Cities of the World</h1>
+      <div className="wrapper">
+        <div className="thumbnails">
+        {images}
+      </div>
+        {<img id="bigimage" src={bigImage}/>}
+      </div>
+</div>
   );
-}
+};
 
 export default App;
